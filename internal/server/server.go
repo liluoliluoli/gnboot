fea97699019ff15a6731584c941c04d157f07cfa
liluoliluoli <liluoliluoli@163.com>
2024-11-24 22:20:24 +0800

    init

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,11 +3,10 @@ package server
 import (
 	"embed"
 	"github.com/google/wire"
-	task2 "gnboot/internal/adaptor/task"
 )
 
 // ProviderSet is server providers.
-var ProviderSet = wire.NewSet(NewGRPCServer, NewHTTPServer, task2.NewWorker)
+var ProviderSet = wire.NewSet(NewGRPCServer, NewHTTPServer, NewWorker)
 
 //go:embed middleware/locales
 var locales embed.FS
